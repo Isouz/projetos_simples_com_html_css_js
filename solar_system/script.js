7b0766841513eff4c05146a081100510831b987c
Igor Souza <103651578+Isouz@users.noreply.github.com>
2023-07-09 17:28:16 -0300

    adição do span

---
+++ solar_system/script.js
@@ -0,0 +1,19 @@
+
+/*
+let active = false
+
+function description() {
+    var buttom = document.getElementsByClassName("material-symbols-outlined")
+    if (active == false) {
+        active = true
+    } else {
+        active = false
+    }
+
+    if (active == true) {
+        mudar botao para ligado
+    } else {
+        mudar botao para desligado
+    }
+}
+*/  
